2661f2e0918c08efc78c8c72dd0352f09dfbd945
Raul <rgomezbe@cisco.com>
2021-10-12 16:36:20 -0500

    commit inicial

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Repositorio en GitHub: https://github.com/rulerdo/buscador_ciudades_dev
+# Clonar repo: git clone git@github.com:rulerdo/buscador_ciudades_dev.git
+# Datos descargados del sitio: https://worldpopulationreview.com/world-cities
+# Repositorio script terminado: https://github.com/rulerdo/buscador_ciudades
+
+import csv, json, sys
+from tabulate import tabulate
+
+def buscar_ciudades():
+
+    pass
+
+def imprimir_busqueda():
+
+    pass
+
+def guardar_busqueda():
+
+    pass
+
+if __name__ == '__main__':
+
+    archivo = input('Archivo origen: ')
+    pais = input('Pais a buscar: ')
+    print(pais,archivo)
